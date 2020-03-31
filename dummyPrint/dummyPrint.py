9cdf188f107013c66c16718a381a9bea0014d98f
Eduardo Aguilar <eduardo.aguilar@mind.be>
2020-03-31 09:22:00 +0200

    dummyPrint.py: catch exception when serial not ready
    Signed-off-by: Eduardo Aguilar <eduardo.aguilar@mind.be>

--- dummyPrint/dummyPrint.py
+++ dummyPrint/dummyPrint.py
@@ -11,19 +11,24 @@ def dummyPrint(device):
     ser.open()
     while True:
         flag = 0
-        ser.write("BPM=100\n".encode('utf-8'))
-        ser.write("VOL=200\n".encode('utf-8'))
-        ser.write("TRIG=0\n".encode('utf-8'))
-        ser.write("PRES=20\n".encode('utf-8'))
-        if ser.in_waiting > 0:
-            rcv = ser.read()
-            if rcv == 'T':
-                flag = 1
-            rcv = ser.read()
-        if flag == 1:
-            ser.write("OK\n".encode('utf-8'))
-        else:
-            ser.write("NOK\n".encode('utf-8'))
+        try:
+            ser.write("BPM=100\n".encode('utf-8'))
+            ser.write("VOL=200\n".encode('utf-8'))
+            ser.write("TRIG=0\n".encode('utf-8'))
+            ser.write("PRES=20\n".encode('utf-8'))
+            if ser.in_waiting > 0:
+                rcv = ser.read()
+                if rcv == 'T':
+                    flag = 1
+                rcv = ser.read()
+            if flag == 1:
+                ser.write("OK\n".encode('utf-8'))
+            else:
+                ser.write("NOK\n".encode('utf-8'))
+        except:
+            print("Serial disconnected or not available")
+            ser.close()
+            break
         time.sleep(0.1)
 
 if __name__ == "__main__":
