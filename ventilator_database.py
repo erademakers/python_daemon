823b73737c7ddb31e87f408b7b31cc3627cc9f25
Merwanski <merwan.birem@hotmail.fr>
2020-04-01 13:22:23 +0200

    added last n data function to the ventilator databse

--- ventilator_database.py
+++ ventilator_database.py
@@ -29,16 +29,27 @@ class DbClient():
         collection = self.db.trigger_values
         self.__store_value(collection, trigger_val)
 
-
     def __store_value(self, collection, val):
         try:
             collection.insert_one({'value': val, 'loggedAt': datetime.utcnow()})
         except errors.ConnectionFailure:
             print("Lost connection, client will attempt to reconnect")
 
+    def last_n_data(self, N, type_data):
+        # B        
+        if type_data == 'BPM':
+            return self.db.breathsperminute_values.find().sort("loggedAt", -1).limit(N)
+        elif type_data == 'VOL':
+            return self.db.volume_values.find().find().sort("loggedAt", -1).limit(N)
+        elif type_data == 'TRIG':
+            return self.db.trigger_values.find().sort("loggedAt", -1).limit(N)
+        elif type_data == 'PRES':
+            return self.db.pressure_values.find().sort("loggedAt", -1).limit(N)
+        else:
+            print("[ERROR] value type not recognized use: BPM, VOL, TRIG, or PRES")
+
     def run(self, name):
         print("Starting {}".format(name))
-
         # Only start MongoClient after fork()
         try:
             self.client = MongoClient(self.addr)
@@ -51,8 +62,6 @@ class DbClient():
                 msg = self.queue.get()
             except queue.Empty:
                 continue
-
-
             try:
                 if msg['type'] == 'BPM':
                     self.store_bpm(msg['val'])
